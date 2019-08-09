93705defdb5128577881d21f6cef6ad637a4497d
RG252 <53072407+RG252@users.noreply.github.com>
2019-08-09 09:32:58 +0530

    Add files via upload

---
+++ arearec.java
@@ -0,0 +1,11 @@
+class rectangle
+{
+public static void main(String[] args)
+{
+double l=5,b=4;
+double area=l*b;
+double peri=2*(l+b);
+System.out.println("Area of rectangle="+area);
+System.out.print("Circumference of rectangle="+peri);
+}
+}
