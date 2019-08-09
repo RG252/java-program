7953557b30d0488ddf6551551653a1aea5d47c0c
RG252 <53072407+RG252@users.noreply.github.com>
2019-08-09 10:54:25 +0530

    Add files via upload

---
+++ greatestnoof2.java
@@ -0,0 +1,18 @@
+class greatestnoof2
+{
+public static void main(String[] args)
+{
+double a=5,b=4;
+if(a>b)
+{
+  if(a>b)
+   {
+    System.out.print("the greatest no="+a);
+   }
+  else  
+   { 
+    System.out.print("the greatest no="+b);
+   }
+}
+}
+}
---
+++ greatestnum.java
@@ -0,0 +1,29 @@
+class greatestno
+{
+public static void main(String[] args)
+{
+double a=5,b=4,c=9;
+if(a>b)
+{
+  if(a>c)
+   {
+    System.out.print("the greatest no="+a);
+   }
+   else
+   {
+    System.out.print("the greatest no="+c); 
+   } 
+}
+else
+{
+  if(b>c)
+   { 
+    System.out.print("the greatest no="+b);
+   }
+  else
+   {
+    System.out.print("the greatest no="+c); 
+   } 
+}
+}
+}
