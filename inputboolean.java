c9126485829f1100697c8fda04ed7bbb00b4f094
RG252 <53072407+RG252@users.noreply.github.com>
2019-08-23 09:47:17 +0530

    Add files via upload

---
+++ inputboolean.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Inputs
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter boolean");
+Boolean a=sc.nextBoolean();
+System.out.println("Your boolean= "+a);
+}
+}
---
+++ inputfloat.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Inputs
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter float");
+float a=sc.nextFloat();
+System.out.println("Your float= "+a);
+}
+}
