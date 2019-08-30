e25b1f90367dc786e972f4f865b99feb1c2c8d55
RG252 <53072407+RG252@users.noreply.github.com>
2019-08-30 10:30:08 +0530

    Add files via upload

---
+++ arraylength.java
@@ -0,0 +1,20 @@
+class array
+{
+public static void main(String[] args)
+{
+int[] steps={1,2,3,4,5,6};
+short[] step1={2,3};
+float[] step2={9.4f,3.6f,12f,4.7f};
+char[] step3={'a','d','g','j','f','g','d','r'};
+long[] step4={6754,987,9865,4567};
+double[] step5={9878,8764,3456,8765};
+boolean[] step6={false,false,true};
+System.out.println("Array length="+steps.length);
+System.out.println("Array length="+step1.length);
+System.out.println("Array length="+step2.length);
+System.out.println("Array length="+step3.length);
+System.out.println("Array length="+step4.length);
+System.out.println("Array length="+step5.length);
+System.out.println("Array length="+step6.length);
+}
+}
